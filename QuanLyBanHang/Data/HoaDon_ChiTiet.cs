b7870e59f9d8c716b66698688fc67caab33782f1
DPM235447_TranLeHuuLy <ly_dpm235447@student.agu.edu.vn>
2026-02-26 15:14:45 +0700

    Buoi 04

--- QuanLyBanHang/Data/HoaDon_ChiTiet.cs
+++ QuanLyBanHang/Data/HoaDon_ChiTiet.cs
@@ -16,5 +16,16 @@ namespace QuanLyBanHang.Data
 
         public virtual HoaDon HoaDon { get; set; } = null!;
         public virtual SanPham SanPham { get; set; } = null!;
+
+        public class DanhSachHoaDon_ChiTiet
+        {
+            public int ID { get; set; }
+            public int HoaDonID { get; set; }
+            public int SanPhamID { get; set; }
+            public string TenSanPham { get; set; } // Thêm 
+            public short SoLuongBan { get; set; }
+            public int DonGiaBan { get; set; }
+            public int ThanhTien { get; set; } // Thêm 
+        }
     }
 }
